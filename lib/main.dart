a6aba7ed0cf561c342b682d9ffd5b92b86eb48f8
zqw <qingwu.zhou@aptsys.com.sg>
2022-03-27 10:17:38 +0800

    Image.network

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
@@ -16,5 +14,55 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  // runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApps());
+}
+
+class MyApps extends StatelessWidget {
+  const MyApps({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Learn Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const MyImage(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+    );
+  }
+}
+
+class MyImage extends StatelessWidget {
+  const MyImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: double.infinity,
+        height: 300,
+        child: Image.network(
+          'http://www.news.cn/photo/2022-03/25/1128501151_16481677708721n.jpg', // this image doesn't exist
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              color: Colors.amber,
+              alignment: Alignment.center,
+              child: const Text(
+                'Whoops!',
+                style: TextStyle(fontSize: 30),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }

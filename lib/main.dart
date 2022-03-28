6c81df9503c098b353f45d83eab5dddeca0cdb0d
zqw <qingwu.zhou@aptsys.com.sg>
2022-03-28 23:00:39 +0800

    List Widget

--- lib/main.dart
+++ lib/main.dart
@@ -43,51 +43,27 @@ class MyApps extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   const HomeContent({Key? key}) : super(key: key);
 
+  List<Widget> _getData() {
+    return const [
+      ListTile(
+        title: Text('I am a List'),
+        subtitle: Text('I am a subtitle'),
+      ),
+      ListTile(
+        title: Text('I am two List'),
+        subtitle: Text('I am a subtitle'),
+      ),
+      ListTile(
+        title: Text('I am three List'),
+        subtitle: Text('I am a subtitle'),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: ListView(
-        padding: const EdgeInsets.all(10),
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          Container(
-            width: 100,
-            color: Colors.red,
-          ),
-          Container(
-            width: 200,
-            color: Colors.yellow,
-            child: ListView(
-              padding: const EdgeInsets.all(20),
-              children: [
-                Image.network(
-                  'https://cdn-icons-png.flaticon.com/512/4193/4193288.png',
-                  height: 100,
-                  fit: BoxFit.fitHeight,
-                ),
-                const Text(
-                  'Panda',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 18,
-                  ),
-                ),
-                Image.network(
-                  'https://cdn-icons-png.flaticon.com/512/4193/4193310.png',
-                  height: 100,
-                  fit: BoxFit.fitHeight,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 100,
-            color: Colors.blue,
-          ),
-        ],
-      ),
+    return ListView(
+      children: _getData(),
     );
   }
 }
